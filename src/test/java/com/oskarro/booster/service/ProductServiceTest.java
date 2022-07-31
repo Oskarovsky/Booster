cb3ee01407122af3c71055fc653aa1fbd63121c2
Oskar Słyk <oskar.slyk@mavoco.com>
2022-07-31 15:02:35 +0200

    Repo testing

--- src/test/java/com/oskarro/booster/service/ProductServiceTest.java
+++ src/test/java/com/oskarro/booster/service/ProductServiceTest.java
@@ -1,19 +1,13 @@
 package com.oskarro.booster.service;
 
 import com.oskarro.booster.config.SpringDataConfiguration;
-import com.oskarro.booster.model.Meal;
 import com.oskarro.booster.model.Product;
-import com.oskarro.booster.repository.MealRepository;
-import com.oskarro.booster.repository.ProductRepository;
-import org.hibernate.Hibernate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.transaction.Transactional;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -47,25 +41,4 @@ public class ProductServiceTest {
                 () -> assertEquals("Hazel", products.get(0).getName())
         );
     }
-
-    @Test
-    @Transactional
-    public void test_saveListOfMealsForOneProduct() {
-        // GIVEN
-        Product product = Product.builder().name("Water").energy(0).fat(0d).build();
-        Product productCurrent = productService.create(product);
-
-        // WHEN
-        Meal mealOne = Meal.builder().dateTime(LocalDateTime.now()).portion(2d).product(productCurrent).build();
-        Meal mealTwo = Meal.builder().dateTime(LocalDateTime.now()).portion(3d).product(productCurrent).build();
-        Meal mealThree = Meal.builder().dateTime(LocalDateTime.now()).portion(24d).product(productCurrent).build();
-        mealService.create(mealOne);
-        mealService.create(mealTwo);
-        mealService.create(mealThree);
-        productCurrent = productService.getById(productCurrent.getId());
-
-        // THEN
-        assertEquals(productCurrent.getMeals().size(), 3);
-    }
-
 }
